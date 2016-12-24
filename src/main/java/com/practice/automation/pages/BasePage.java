8cc9e77ff4d0b48ea3b58c675cefa8fea45640ce
Chandra Deo Narayan <chandrad@thoughtworks.com>
2016-12-24 11:33:27 +0530

    added pages for page object pattern

---
+++ src/main/java/com/practice/automation/pages/BasePage.java
@@ -0,0 +1,9 @@
+package com.practice.automation.pages;
+
+/**
+ * Created by chandrad on 12/24/16.
+ */
+public class BasePage {
+
+
+}
---
+++ src/main/java/com/practice/automation/pages/OrderHistoryPage.java
@@ -0,0 +1,7 @@
+package com.practice.automation.pages;
+
+/**
+ * Created by chandrad on 12/24/16.
+ */
+public class OrderHistoryPage {
+}
